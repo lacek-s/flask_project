764d4573ff74236c00b805fce1fe09f5952b33a2
Sonalacek <barancikova.s@gmail.com>
2025-11-27 09:02:39 +0000

    Adds error handlers

--- configs/default.py
+++ configs/default.py
@@ -1,3 +1,5 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+
 SECRET_KEY = b'`\xce\xd3\x06\x07X\xb5<\xc6-\xbc!\r\x99\xad\x1cX\xb1\x8e&`\x97\xd6\xe1'
-DATABASE = "/var/www/mdblog/blog.db"
+# DATABASE = "/var/www/mdblog/blog.db"
 
